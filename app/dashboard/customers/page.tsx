4b4aef20a34b76ce8552c4b696e9200295de645c
pantharev <narev001@fiu.edu>
2023-12-03 15:54:09 -0500

    added customers table with back-end sql logic and pagination. wip create customer button

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,38 @@
-export default function Page() {
-    return <p>Customers Page</p>;
+import { fetchFilteredCustomers, fetchCustomersPages } from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
+import { lusitana } from "@/app/ui/fonts";
+import Pagination from "@/app/ui/invoices/pagination";
+import Search from "@/app/ui/search";
+import { CustomersTableSkeleton } from "@/app/ui/skeletons";
+import { Suspense } from "react";
+
+export default async function Page({ searchParams }: { searchParams?: {
+    query?: string;
+    page?: string;
+}}) {
+    const query = searchParams?.query || '';
+    const currentPage = Number(searchParams?.page) || 1;
+
+    const totalPages = await fetchCustomersPages(query);
+    const customers = await fetchFilteredCustomers(query, currentPage);
+
+    return (
+        <div className="w-full">
+            <div className="flex w-full items-center justify-between">
+                <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+                </div>
+                <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+                    <Search placeholder="Search customers..." />
+                    {/* <CreateCustomer /> */}
+                    <div className="rounded-md bg-blue-400 hover:bg-blue-600 cursor-pointer p-2">Create Customer</div>
+                    </div>
+                <Suspense key={query} fallback={<CustomersTableSkeleton />}>
+                    <CustomersTable customers={customers} />
+                </Suspense>
+            <div className="mt-5 flex w-full justify-center">
+                <Pagination totalPages={totalPages} />
+            </div>
+        </div>
+    )
+                            ;
 }
--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -10,10 +10,10 @@ export default async function CustomersTable({
 }) {
   return (
     <div className="w-full">
-      <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
+      {/* <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
         Customers
       </h1>
-      <Search placeholder="Search customers..." />
+      <Search placeholder="Search customers..." /> */}
       <div className="mt-6 flow-root">
         <div className="overflow-x-auto">
           <div className="inline-block min-w-full align-middle">
